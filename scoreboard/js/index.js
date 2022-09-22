56659849c120eb2dff7b2de2bee9820e42eadcdd
Kanav Arora <arora.kanav777@gmail.com>
2022-09-22 14:08:23 +0530

    Project ScoreBoard
    Project  Name- ScoreBoard
    Aim - Basics of js , Learn and UI designing

---
+++ scoreboard/js/index.js
@@ -0,0 +1,88 @@
+
+
+let homescore = 0
+
+let guestscore = 0
+
+/*
+
+
+ID AS A PARAMETER 
+
+
+
+function addScore(){
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ */
+
+
+function hp1(){
+
+    homescore+=1
+    document.getElementById("hscore").innerText=homescore
+}
+
+function hp2(){
+
+    homescore+=2
+    document.getElementById("hscore").innerText=homescore
+}
+
+function hp3(){
+
+    homescore+=3
+    document.getElementById("hscore").innerText=homescore
+}
+
+
+function gp1(){
+
+    homescore+=1
+    document.getElementById("gscore").innerText=homescore
+}
+
+function gp2(){
+
+    homescore+=2
+    document.getElementById("gscore").innerText=homescore
+}
+
+function gp3(){
+
+    homescore+=3
+    document.getElementById("gscore").innerText=homescore
+}
+
+
